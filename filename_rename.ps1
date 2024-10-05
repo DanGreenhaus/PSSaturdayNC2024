2aaaa063ebd6ba1d05311c3f47edbbade8aeac86
DanGreenhaus <88389380+DanGreenhaus@users.noreply.github.com>
2024-10-04 22:20:34 -0400

    Create filename_rename.ps1

---
+++ filename_rename.ps1
@@ -0,0 +1,54 @@
+#to remove a phrase that has been appended to the end of a bunch of file names
+$dir = "<insert folder path of files here>" #this is the folder location that needs to be replaced
+$phrase = " <Replace this stuff>" #this with phrase you are looking to remove
+#remove the last number of characters in the file names
+$length=$phrase.Length 
+Get-ChildItem $dir -Recurse| Where-Object { $_.Name -like "*$phrase*" } |
+  Rename-Item -NewName { $_.name.substring(0,$_.BaseName.length-$length)+$_.Extension}# -WhatIf -verbose
+
+#if you have multiple items:
+$phrases=@(
+    "<phrase 1>"
+    "<Phrase 2>" 
+    "<etc>" 
+)
+foreach ($phrase in $phrases){
+  Get-ChildItem $dir -Recurse| Where-Object { $_.Name -like "*$phrase*" } |
+   Rename-Item -NewName { $_.Name -replace [regex]::Escape($Phrase),""}# -WhatIf 
+}
+
+#if you need to eliminate the _ in a filename, and replace it with a space
+Get-ChildItem $dir -Recurse| Where-Object { $_.Name -like "*$phrase*" } | 
+Rename-Item -NewName { $_.Name -replace "_"," " }
+
+#to rename certain files that meet case criteria
+Get-ChildItem $dir -Recurse| Where-Object { $_.Name -clike "*$phrase*" } |
+ Rename-Item -NewName { $_.Name -replace "Deadly Class v","Deadly Cass Vol. " }
+
+#these commands will find folders with a particular phrase in the name
+Get-ChildItem $dir -recurse -filter $phrase -Directory | ForEach-Object { $_.fullname }
+Get-ChildItem $dir -recurse | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "$phrase"}
+Get-ChildItem $dir -recurse -filter "<search criteria>" | Remove-Item
+
+#to list all folders in a directory that match a particular phrase
+Get-ChildItem $dir *$phrase* -Recurse -Directory
+
+#if you have folders in the zip files, use this command to extract them into one main folder
+Get-ChildItem $dir\zipfiles\*.* -recurse | Move-Item -Destination "$dir\zipfiles\files"
+
+#or
+Get-ChildItem "$dir\*.zip" -Recurse |Expand-Archive -DestinationPath "$dir\" -Force #unzips files into root folder
+Get-ChildItem "$dir\*.zip" -Recurse | Remove-Item #removes the zip files after they are extracted
+
+#to remove trailing spaces
+Get-ChildItem -Path $targetDirectory -File | ForEach-Object {
+  $originalName = $_.Name.TrimEnd()  # Trim trailing spaces first
+  $pattern = "\((.*?)\)"
+  If ($originalName -match $pattern) {
+      $newName = $matches[1] + " - " + $originalName.Replace($matches[0], "").TrimEnd()
+      $newName = $_.Name.TrimEnd() 
+      Rename-Item -Path $_.FullName -NewName $newName -ErrorAction Stop
+      Write-Host "Renamed $_ to $newName"  # Provide feedback on successful renaming
+  }
+}
+
